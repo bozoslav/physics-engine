bcb0ebba20894340d9d046eb5468dcd50647008a
bozoslav <leonmamic111@gmail.com>
2025-04-29 19:45:33 +0200

    added settings

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "test");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
